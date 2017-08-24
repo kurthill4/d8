f2b537fdf9390ab27052fdb3f04e82bdbb25b288
Bill Smith <btsmith@sdccd.edu>
2017-08-24 15:25:39 -0700

    Fixed a lot of annoying jQuery errors. In-place editing now works

--- web/themes/custom/miramar/js/main.js
+++ web/themes/custom/miramar/js/main.js
@@ -23,34 +23,48 @@ function _toggleArrow(ele) {
   })
 
 */
+}(jQuery));
 
   // Searchbar show/hide controls.
-
+(function ($) {
 	Drupal.behaviors.toggleSearch = {
 		attach: function(context,settings) {
-			$('.top-links .search').click(function() {
-				$('.search-desktop').toggle();
+			$('.top-links a.search').unbind('click').click(function() {
+				$('.search-desktop').slideToggle();
 			});
 		}
 	};
+})(jQuery);
 
-  $('.search-desktop a.close-x').click(function() {
-    $(this).parent().fadeToggle();
-  })(jQuery);
-
+(function ($) {
+	Drupal.behaviors.closeSearch = {
+		attach: function(context,settings) {
+		  $('.search-desktop a.close-x').unbind('click').click(function() {
+		    $(this).parent().fadeToggle();
+		  });
+		}
+	};
+})(jQuery);
+/*
   // Campus alert toggle.
   $('.campus-alert .fa-chevron-up').click(function() {
     $('.campus-alert, .ca-block').fadeToggle('slow');
-  });
+  }
+})(jQuery);
+
+(function ($) {
 
   // Up/down arrow on mobile for Important Dates.
   $('.important-dates .fa-chevron-up').click(function() {
     $(this).next().next().slideToggle('fast', function() {
       _toggleArrow($(this).prev().prev());
     });
-  });
+  }
+})(jQuery);
+
 
   // Up/down arrow on mobile for News-and-Events and Most-recent-jobs-internships and ID block.
+(function ($) {
   var i = 1;
   $('.news-block .fa-chevron-up, .recent-jobs .fa-chevron-up, .id-block .fa-chevron-up').click(function() {
     $(this).next().children().not(':first').slideToggle('fast', function() {
@@ -59,12 +73,14 @@ function _toggleArrow(ele) {
       i++;
     }(jQuery));
     i = 1;
-  });
+  }
+})(jQuery);
 
   // Up/down arrow on mobile for Mission Statement and Recent Tweets.
+(function ($) {
   $('.mr-block .fa-chevron-up').click(function() {
     $(this).parent().find('p').slideToggle('fast', function() {
        _toggleArrow($(this).parent().find('i'));
-    });
+    }
   });
-})(jQuery);
+})(jQuery);*/
