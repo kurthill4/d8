c35b3f539b8fd8920351e1c1e54d3c4266733e32
Bill Smith <btsmith@sdccd.edu>
2017-11-29 11:20:17 -0800

    Fixing large screen display issue.

--- web/themes/custom/miramar/js/main.js
+++ web/themes/custom/miramar/js/main.js
@@ -15,6 +15,17 @@ var pushDown = document.getElementById("push");
   })
 }(jQuery));
 
+
+/*
+(function(){
+        $('.slider').slick({
+            autoplay:true,
+            arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+        });
+    }(jQuery));
+*/
 (function($) {
   // Mobile Menu controls.
   /*$('.mobile-nav').hide();
