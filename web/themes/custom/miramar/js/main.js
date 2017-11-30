d61209a616221a52993cdf4ff4f020c565b04db9
Bill Smith <btsmith@sdccd.edu>
2017-11-30 14:52:24 -0800

    Interactive map link added to footer.

--- web/themes/custom/miramar/js/main.js
+++ web/themes/custom/miramar/js/main.js
@@ -17,7 +17,7 @@ var pushDown = document.getElementById("push");
 
 
 /*(function($) {
-        $('.slick-views-program-carousel-block-1-1-slider').slick({
+        $('.slick').slick({
 	    arrows:true,
             prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
             nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
