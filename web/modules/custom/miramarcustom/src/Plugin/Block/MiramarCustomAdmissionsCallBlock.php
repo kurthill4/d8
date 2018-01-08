a4c97c675183e5b4d1c2d383843695da8a8096d2
Bill Smith <btsmith@sdccd.edu>
2018-01-08 10:46:54 -0800

    Added call to action on Admissions pages.

---
+++ web/modules/custom/miramarcustom/src/Plugin/Block/MiramarCustomAdmissionsCallBlock.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\miramarcustom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Component\Utility\Unicode;
+
+/**
+ * Provides a 'Call to Action' block.
+ *
+ * @Block(
+ *   id = "miramar_admcall",
+ *   admin_label = @Translation("Admissions Page Call to Action"),
+ * )
+ */
+class MiramarCustomAdmissionsCallBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $imagepath = base_path() . drupal_get_path('theme', 'miramar');
+
+    $build['admcall']['content'] = [
+      '#markup' => '
+	<div class="call">
+	<a href="https://www.sdccd.edu/students/transcripts/" target="_blank">
+	<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+	Transcripts
+	</a>
+	</div>
+	<div class="spacer" style="height:0.1em">&nbsp;</div>
+	<div class="call">
+	<a href="https://www.sdccd.edu/students/fees.aspx">
+	<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+	Tuition &amp; Fees
+	</a>
+	</div>'
+    ];
+
+    return $build;
+  }
+}
