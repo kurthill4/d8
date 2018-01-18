ec376fabe3ea253d369f1c56e12befe5448c143d
Bill Smith <btsmith@sdccd.edu>
2018-01-18 10:52:06 -0800

    Added call to action for Financial Aid

---
+++ web/modules/custom/miramarcustom/src/Plugin/Block/MiramarCustomFinancialCallBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\miramarcustom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Component\Utility\Unicode;
+
+/**
+ * Provides a 'Call to Action' block.
+ *
+ * @Block(
+ *   id = "miramar_fincall",
+ *   admin_label = @Translation("Financial Aid Page Call to Action"),
+ * )
+ */
+class MiramarCustomFinancialCallBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $imagepath = base_path() . drupal_get_path('theme', 'miramar');
+
+    $build['fincall']['content'] = [
+      '#markup' => '
+	<div class="call">
+	<a href="http://www.fafsa.gov" target="_blank">
+	<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+	Apply for Aid
+	</a>
+	</div>'
+    ];
+
+    return $build;
+  }
+}
