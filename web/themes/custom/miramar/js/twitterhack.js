717d2733e154c5e0e450867b29504db4f1c050ad
Bill Smith <btsmith@sdccd.edu>
2018-01-26 15:00:34 -0800

    Adjustment to list margins. Added Twitter Javascript to control display

--- web/themes/custom/miramar/js/twitterhack.js
+++ web/themes/custom/miramar/js/twitterhack.js
@@ -12,9 +12,34 @@ if (ifrm !== null){
   if (timelineWidget !== undefined){
     // then change background color css for this class
     timelineWidget.style.background="none";
+var widget = ifrm.contentWindow.document.getElementsByClassName('timeline-Widget')[0];
+var header = ifrm.contentWindow.document.getElementsByClassName('timeline-Header')[0];
+var footer = ifrm.contentWindow.document.getElementsByClassName('timeline-Footer')[0];
+var brand = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-brand')[0];
+var tweet = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet')[0];
+var author = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-author')[0];
+var media = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-media')[0];
+var meta = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-metadata')[0];
+var body = ifrm.contentWindow.document.getElementsByClassName('timeline-Body')[0];
+var content = ifrm.contentWindow.document.getElementsByClassName("timeline-Tweet-text")[0];
 var image = ifrm.contentWindow.document.getElementsByClassName('MediaCard-media')[0];
-image.style = "-webkit-transform: scale(0.75);-moz-transform: scale(0.75);-ms-transform: scale(0.75);-o-transform: scale(0.75);transform: scale(0.75)";
-
+var border = ifrm.contentWindow.document.getElementsByClassName('MediaCard')[0];
+var action = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-action');
+image.style = "display:none;";//"-webkit-transform: scale(0.75);-moz-transform: scale(0.75);-ms-transform: scale(0.75);-o-transform: scale(0.75);transform: scale(0.75)";
+//timelineWidget.style="height:100%";
+body.style="border:none";
+widget.style="background:none";
+header.style = "display:none";
+brand.style = "display:none";
+author.style = "display:none";
+media.style = "padding:0;height:auto;margin-bottom:0;";
+border.style = "max-height:60%;margin-bottom:0;padding:0";
+meta.style = "display:none";
+footer.style = "padding-top:0;font-weight:bold;font-size:1em";
+content.style="line-height:100%;-webkit-margin-after:0em;margin-bottom:2px;font-family:'PT',serif;font-style:italic!important;font-size:300%!important;border:none";
+for(i=0;i<action.length;i++) {
+	action[i].style = "display:none";
+}
     // and leave setinterval
       clearInterval(myInterval);
       } else {
