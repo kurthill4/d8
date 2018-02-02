7d34fe812b6e438634d7cd412c684992d45cda6d
Bill Smith <btsmith@sdccd.edu>
2018-02-02 10:33:49 -0800

    Adjusted Twitter homepage display

--- web/themes/custom/miramar/js/twitterhack.js
+++ web/themes/custom/miramar/js/twitterhack.js
@@ -17,10 +17,12 @@ var header = ifrm.contentWindow.document.getElementsByClassName('timeline-Header
 var footer = ifrm.contentWindow.document.getElementsByClassName('timeline-Footer')[0];
 var brand = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-brand')[0];
 var tweet = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet')[0];
-var author = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-author')[0];//
+var author = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-author')[0];
 var meta = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-metadata')[0];
 var body = ifrm.contentWindow.document.getElementsByClassName('timeline-Body')[0];
+var viewport = ifrm.contentWindow.document.getElementsByClassName('timeline-Viewport')[0];
 var content = ifrm.contentWindow.document.getElementsByClassName("timeline-Tweet-text")[0];
+var retweet = ifrm.contentWindow.document.getElementsByClassName("timeline-Tweet-retweetCredit")[0];
 if(ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-media').length > 0) {
 	var media = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-media')[0];
 	var border = ifrm.contentWindow.document.getElementsByClassName('MediaCard')[0];
@@ -31,14 +33,16 @@ if(ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-media').le
 }
 var action = ifrm.contentWindow.document.getElementsByClassName('timeline-Tweet-action');
 timelineWidget.style="height:100%";
-body.style="border:none;height:inherit";
+body.style="border:none;";
 widget.style="background:none";
 header.style = "display:none";
+retweet.style = "display:none";
 brand.style = "display:none";
 author.style = "display:none";
 meta.style = "display:none";
-footer.style = "padding-top:0;font-weight:bold;font-size:1em";
-content.style="line-height:100%;height:auto;-webkit-margin-after:0;margin-bottom:2px;font-family:'PT',serif;font-style:italic!important;font-size:300%!important;border:none";
+footer.style = "display:block;margin-top:1em;font-weight:bold;font-size:1em";
+viewport.style="-webkit-margin-after:0.5em!important";
+content.style="line-height:100%;font-family:'PT',serif;font-style:italic!important;font-size:250%!important;border:none";
 for(i=0;i<action.length;i++) {
 	action[i].style = "display:none";
 }
