27ff5dbf7ca816fbe81f13d71252720f036cdd8b
Bill Smith <btsmith@sdccd.edu>
2018-06-11 15:17:17 -0700

    Updated link in call to action block. Added Outreach form

--- web/themes/custom/miramar/js/main.js
+++ web/themes/custom/miramar/js/main.js
@@ -52,6 +52,23 @@ var pushDown = document.getElementById("push");
 })(jQuery);
 
 
+(function ($) {
+/*	Drupal.behaviors.trackem = {
+		attach: function(context,settings) {
+			context.once(function() {
+				$('.slick-1 a').on('click',function() {
+					_gaq.push([‘_trackEvent’, ‘downloads’, ‘click’, ‘whitepaper’, ‘0’]);
+				}
+			}
+		}
+	}*/
+		  $('.slick-1 a').on('click',function() {
+			_gaq.push([‘_trackEvent’, ‘downloads’, ‘click’, ‘whitepaper’, ‘0’]);
+			}
+		);
+})(jQuery);
+
+
 
 /*
  (function ($, Drupal, settings) {
