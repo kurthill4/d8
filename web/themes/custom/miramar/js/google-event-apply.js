e0995beb4638b2b198ebbc082dc85128724150b1
Bill Smith <btsmith@sdccd.edu>
2019-06-28 11:49:46 -0700

    Cleaning up Google tracking code for non-step Apply links

--- web/themes/custom/miramar/js/google-event-apply.js
+++ web/themes/custom/miramar/js/google-event-apply.js
@@ -4,6 +4,6 @@
 
  */
 (function ($) {
-	$('a[href$="apply.sdccd.edu"]').not("div.step-desc a").on('click',function(){ga('send','event','Apply_NonStep','ApplyNow','Apply_Now_Link')});
+	$('a[href*="apply.sdccd.edu"]').not("div.step-desc a").on('click',function(){ga('send','event','Apply_NonStep','ApplyNow','Apply_Now_Link')});
 }(jQuery));
 
