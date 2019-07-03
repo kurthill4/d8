16bedafb29c4b4f4dc0cc4485a3cf68dc3910249
Bill Smith <btsmith@sdccd.edu>
2019-07-03 11:34:23 -0700

    Added Steps for graduating students go Google Analytics. Minor tweaks to field validation

---
+++ web/themes/custom/miramar/js/google-event-grad-steps.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Send event info to Google for Graduating Students pathway.
+ *
+ */
+
+var step = [
+	['/campus/counseling','Eligibility'],
+	['/campus/graduation/steps-to-graduate','Apply_To_Graduate'],
+	['/campus/graduation/getgear','Gear'],
+	['/campus/graduation/checklist','Checklist'],
+	['http://alumni.sdccd.edu/?page=Miramar','Alumni']
+];
+
+(function ($) {
+	for(var i=0;i<step.length;i++) {
+		url = step[i][0];
+		action = step[i][1];
+		label = 'Graduating_Students_Step_' + (i + 1);
+		$('.step-desc a[href*="' + url + '"]').on('click',{a: action,l: label},function(e){ga('send','event',e.data.a,e.data.l);});
+	}
+}(jQuery));
+
+
+
+
+
+
