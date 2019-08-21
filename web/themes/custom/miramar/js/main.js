b2aab3789fee0f56ced3b79db0de4549b7c97857
Bill Smith <btsmith@sdccd.edu>
2019-08-21 13:51:08 -0700

    More adjustments to planning pages. Added template for third prototype. Adjusted JavaScript for IE display

--- web/themes/custom/miramar/js/main.js
+++ web/themes/custom/miramar/js/main.js
@@ -19,6 +19,66 @@ var pushDown = document.getElementById("push");
 }(jQuery));
 
 
+
+
+/**
+
+	Get browser version and do browser-dependent CSS settings\
+
+**/
+
+(function($) {
+	var BrowserDetect = {
+		init: function () {
+		    this.browser = this.searchString(this.dataBrowser) || "Other";
+		    this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "Unknown";
+		},
+		searchString: function (data) {
+		    for (var i = 0; i < data.length; i++) {
+		        var dataString = data[i].string;
+		        this.versionSearchString = data[i].subString;
+
+		        if (dataString.indexOf(data[i].subString) !== -1) {
+		            return data[i].identity;
+		        }
+		    }
+		},
+		searchVersion: function (dataString) {
+		    var index = dataString.indexOf(this.versionSearchString);
+		    if (index === -1) {
+		        return;
+		    }
+
+		    var rv = dataString.indexOf("rv:");
+		    if (this.versionSearchString === "Trident" && rv !== -1) {
+		        return parseFloat(dataString.substring(rv + 3));
+		    } else {
+		        return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+		    }
+		},
+
+		dataBrowser: [
+		    {string: navigator.userAgent, subString: "Edge", identity: "MS Edge"},
+		    {string: navigator.userAgent, subString: "MSIE", identity: "Explorer"},
+		    {string: navigator.userAgent, subString: "Trident", identity: "Explorer"},
+		    {string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
+		    {string: navigator.userAgent, subString: "Opera", identity: "Opera"},  
+		    {string: navigator.userAgent, subString: "OPR", identity: "Opera"},  
+
+		    {string: navigator.userAgent, subString: "Chrome", identity: "Chrome"}, 
+		    {string: navigator.userAgent, subString: "Safari", identity: "Safari"}       
+		]
+	    };
+	    
+	BrowserDetect.init();
+
+	if(BrowserDetect.browser == 'Explorer') {
+		$('.planning svg').attr({"width":"1377.73","height":"826.23175"});
+	}
+}(jQuery));
+
+
+
 /*(function($) {
         $('.slick').slick({
 	    arrows:true,
