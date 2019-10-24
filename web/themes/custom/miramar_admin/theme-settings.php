478c6fc5c399783bac32ee954d7a9592292c7f79
Bill Smith <btsmith@sdccd.edu>
2019-10-24 16:28:39 -0700

    Removed unused themes

--- web/themes/custom/miramar_admin/theme-settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @file
- * The theme settings functions.
- */
-
-require_once dirname(__FILE__) . '/includes/fortytwo_admin.inc';
-require_once dirname(__FILE__) . '/includes/theme-settings-general.inc';
