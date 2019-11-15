b36d8f34321432caa08b76a2ecf0654de640ba9f
Bill Smith <btsmith@sdccd.edu>
2019-11-15 15:39:58 -0800

    Refined Google Analytics for call to action

--- web/themes/custom/miramar/js/google-event-calltoaction.js
+++ web/themes/custom/miramar/js/google-event-calltoaction.js
@@ -17,4 +17,10 @@
 		$(this).on('click',{ a: label },function(e){ga('send','event','Call to Action--Nav Bar','Homepage',e.data.a)});
 	});
 }(jQuery));
+(function ($) {
+	$('.adbuttons a,.adtext a').each(function() {
+		label = $(this).text();
+		$(this).on('click',{ a: label },function(e){ga('send','event','Call to Action--Ad Button','Homepage',e.data.a)});
+	});
+}(jQuery));
 
